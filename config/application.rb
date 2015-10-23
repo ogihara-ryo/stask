5e54dd90047472efb82b3f09d2bd60a69571add6
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-23 22:44:46 +0900

    config/application.rb にアプリケーションのタイトルを定義

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Stask
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # アプリケーションのタイトルを定義
+    config.title = 'S Task'
   end
 end
