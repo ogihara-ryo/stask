233649e9298bf952f50363fe6dd8700b2896a46c
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-23 22:46:47 +0900

    config/application.rb にデフォルトのテンプレートエンジンを定義(Slim)

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module Stask
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    # デフォルトのテンプレートエンジンを Slim に変更
+    config.generators.template_engine = :slim
+
     # アプリケーションのタイトルを定義
     config.title = 'S Task'
   end
