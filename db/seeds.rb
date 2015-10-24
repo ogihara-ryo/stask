841585042567baaf537a07bccbbd91a5804ff724
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-25 01:00:02 +0900

    db/seeds.rb に categories テーブルのサンプルを実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create([
+  { name: 'カテゴリー1', color: '#d8eecf' },
+  { name: 'カテゴリー2', color: '#d1e8f4' },
+  { name: 'カテゴリー3', color: '#fbf7dc' },
+  { name: 'カテゴリー4', color: '#efd6d6' }
+])
