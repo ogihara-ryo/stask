06e02a46e0e509ca81956c6e23445ab142f91ff2
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-24 13:54:56 +0900

    annotate 実行

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  color      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Category < ActiveRecord::Base
   has_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id            :integer          not null, primary key
+#  target        :date             not null
+#  name          :string           not null
+#  priority      :integer
+#  category_id   :integer
+#  estimate_hour :float
+#  started_at    :datetime
+#  finished_at   :datetime
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Task < ActiveRecord::Base
   belongs_to :category
 end
