21ec6e77837c4e3c544eaa1ac7b942828aed75a1
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-29 20:17:06 +0900

    spec/factories/categories.rb に annotate

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  number     :integer          not null
+#  name       :string           not null
+#  color      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :category do
     sequence(:number)
