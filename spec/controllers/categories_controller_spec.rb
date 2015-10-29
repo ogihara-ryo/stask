475ac1a9fd095e37bcb1d65679fbbed1d5f4bf90
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-29 21:44:00 +0900

    categories_controller_spec.rb 実装

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe CategoriesController, type: :controller do
     it { is_expected.to have_http_status(:ok) }
     it { is_expected.to render_template(:index) }
   end
+
+  describe 'GET #' do
+    context '有効なカテゴリーIDを指定された場合' do
+      let!(:category) { FactoryGirl.create(:category, number: 1, name: 'Category', color: '#000000') }
+      before { xhr :get, :set_id, id: category.id }
+      it { is_expected.to have_http_status(:ok) }
+      it { expect(assigns(:category)).to eq(category) }
+    end
+
+    context '無効なカテゴリーIDを指定された場合' do
+      before { xhr :get, :set_id, id: 1 }
+      it { is_expected.to have_http_status(:ok) }
+    end
+  end
 end
