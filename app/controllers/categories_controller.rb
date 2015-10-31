5a24271a3b1b22609325c6fc09866da2e1424c80
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-31 15:06:10 +0900

    CategoriesController#destroy の before_action に set_categories を設定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_categories, only: %i(index create)
+  before_action :set_categories, only: %i(index create destroy)
 
   def index
     @category = Category.new
