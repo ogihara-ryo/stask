25fc48c52edbf5f0c74d995ec89da9cae0ca1293
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-31 20:18:58 +0900

    database_cleaner の設定を spec_helper.rb から rails_helper.rb へ移動

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,18 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  # database_cleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
