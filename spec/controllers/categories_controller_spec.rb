bd29f6a7703bce43715c80196cdbff1a7876ce7c
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-10-31 14:14:04 +0900

    CategoriesController#create の正常系テスト実装

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -9,6 +9,27 @@ RSpec.describe CategoriesController, type: :controller do
     it { is_expected.to render_template(:index) }
   end
 
+  describe 'POST #create' do
+    context '存在しない ID を指定された場合' do
+      it 'Category に新しいレコードが登録されること' do
+        expect {
+          xhr :post, :create, category: FactoryGirl.attributes_for(:category)
+        }.to change(Category, :count).by(1)
+      end
+    end
+
+    context '存在する ID を指定された場合' do
+      it '既存のレコードが更新されること' do
+        category = FactoryGirl.create(:category)
+        xhr :post, :create, category: { id: category.id, number: 2, name: 'test', color: '#ffffff' }
+        category.reload
+        expect(category.number).to eq 2
+        expect(category.name).to eq 'test'
+        expect(category.color).to eq '#ffffff'
+      end
+    end
+  end
+
   describe 'GET #set_id' do
     context '有効なカテゴリーIDを指定された場合' do
       let!(:category) { FactoryGirl.create(:category, number: 1, name: 'Category', color: '#000000') }
