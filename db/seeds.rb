15b893597f325fb2e23d07cc48f6f3bb529e8010
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-11-03 00:58:16 +0900

    seed ファイルに Task を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.delete_all
 Category.create([
   { number: 1, name: 'カテゴリー1', color: '#d8eecf' },
   { number: 2, name: 'カテゴリー2', color: '#d1e8f4' },
   { number: 3, name: 'カテゴリー3', color: '#fbf7dc' },
   { number: 4, name: 'カテゴリー4', color: '#efd6d6' }
 ])
+
+Task.delete_all
+1.upto(4) do |i|
+  # 昨日
+  Task.create(id_per_days: i, target: Time.zone.yesterday, name: "タスク#{i}", priority: i,
+              category: Category.find_by(number: i),
+              estimate_hour: i + 0.5, started_at: (5 - i).hour.ago, finished_at: (4 - i).hour.ago)
+  # 今日
+  Task.create(id_per_days: i, target: Time.zone.today, name: "タスク#{i}", priority: i,
+              category: Category.find_by(number: i),
+              estimate_hour: i + 0.5, started_at: (5 - i).hour.ago)
+  # 明日
+  Task.create(id_per_days: i, target: Time.zone.tomorrow, name: "タスク#{i}", priority: i,
+              category: Category.find_by(number: i),
+              estimate_hour: i + 0.5, started_at: (5 - i).hour.ago)
+end
