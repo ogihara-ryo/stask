5e68020533d9448b8eaf69790b504fe3e3550bb2
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2015-11-08 16:35:21 +0900

    categories#index でレコードが存在しないパターンの想定

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_categories, only: %i(index create destroy)
 
   def index
-    @category = Category.first
+    @category = Category.first || Category.new
   end
 
   def create
